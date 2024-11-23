9f86220ac8eef2b151dc7d85e33bbc6e5dddc148
Grigory P <grisha9871234@gmail.com>
2024-11-23 15:55:11 +0300

    Snils checking

--- Laba2.py
+++ Laba2.py
@@ -3,16 +3,24 @@ import unittest
 
 """СНИЛС ВИДА XXX-XXX-XXX XX"""
 
-with open("random_snils.txt", "w", encoding="utf-8") as file:
-    file.write("""123-456-789 01
-987-654-321 99
-000-000-000 00
-123-456-78 90
-456-123-78910
-abc-def-ghi jk
-123-456-789-01
-32165498700
-999-888-777 11
-111-222-333 44
-""")
-print("Файл random_snils.txt успешно создан.")
+SNILS_REGEX = r"^\d{3}-\d{3}-\d{3} \d{2}$"
+filename = 'random_snils.txt'
+
+# Функция проверки СНИЛС
+def is_valid_snils(snils):
+    """Проверяет, соответствует ли СНИЛС формату."""
+    return re.match(SNILS_REGEX, snils) is not None
+
+# Функция поиска СНИЛС в файле
+def find_snils_in_file(filename):
+    """Ищет все подходящие СНИЛС в файле."""
+    snils_list = []
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip("\n")
+            if is_valid_snils(line):
+                snils_list.append(line)
+    return snils_list
+
+found_snils = find_snils_in_file(filename)
+print("Найденные СНИЛС:", found_snils)
