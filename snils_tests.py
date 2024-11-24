bd6bdd93fcde8c58f4d14fbe25b35a5d7ebbbff7
Grigory P <grisha9871234@gmail.com>
2024-11-24 11:51:07 +0300

    Unit test start

--- snils_tests.py
+++ snils_tests.py
@@ -3,13 +3,16 @@ from Laba2 import is_valid_snils
 
 # Правильный ли снилс или нет
 class TestSnilsFunctions(unittest.TestCase):
-    def test_is_valid_snils(self):
-        # Правильно
+
+    # Тесты на правильный снилс
+    def test_valid_snils(self):
         self.assertTrue(is_valid_snils("123-456-789 01"))
         self.assertTrue(is_valid_snils("880-055-535 35"))
         self.assertTrue(is_valid_snils("666-666-666 36"))
         self.assertTrue(is_valid_snils("000-000-000 00"))
-        # Неправильно
+
+    # Тесты на неправильный снилс
+    def test_invalid_snils(self):
         self.assertFalse(is_valid_snils("12345678901"))
         self.assertFalse(is_valid_snils("123-45-6789 01"))
         self.assertFalse(is_valid_snils("TUS-OVA-ONE <3"))
